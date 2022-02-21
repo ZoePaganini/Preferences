a840a0933158125333090dd11361e93b16f5e2a7
unknown <zoepaganini@paucasesnovescifp.cat>
2022-02-21 19:28:40 +0100

    Ejercicio de preferencias para poder almacenar datos aun sin cerrando la app y volviendo a iniciarla

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:preferences_app/widgets/widgets.dart';
+
+import '../shared_preferences/preferences.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  static const String routerName = 'home';
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      drawer: const SideMenu(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:  [
+          Text('Dark Mode: ${Preferences.isDarkMode}'),
+          Divider(),
+          Text('Gènere: ${Preferences.genere}'),
+          Divider(),
+          Text('Nom d\'usuari: ${Preferences.nom}'),
+          Divider()
+        ],
+      )
+    );
+  }
+}
